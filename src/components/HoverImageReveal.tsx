e9466a8483f8ed1801c544d5508422ee855d0095
vinamra1102 <vinamra.229302343@muj.manipal.edu>
2026-09-13 21:24:45 +0530

    Revert "fix: expand block media to text ratio 40 60"
    This reverts commit 08e199fdef42a4562020cea10f53b2fa9a63182d.

--- src/components/HoverImageReveal.tsx
+++ src/components/HoverImageReveal.tsx
@@ -467,7 +467,7 @@ export default function HoverImageReveal({
                   {/* Media panel: a tall narrow card inset 12px from the top and bottom. */}
                   <div
                     style={{
-                      width: "40%",
+                      width: "200px",
                       flexShrink: 0,
                       alignSelf: "stretch",
                       margin: "12px 0",
